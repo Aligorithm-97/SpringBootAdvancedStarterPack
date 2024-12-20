36148302b8e3ad295bec6e78583e2c1fc5f889bb
Aligorithm-97 <atemurtas97@gmail.com>
2024-12-20 20:23:54 +0300

    aligorithm-mapper

--- src/main/java/com/spring/temp/domain/service/impl/ExampleServiceImpl.java
+++ src/main/java/com/spring/temp/domain/service/impl/ExampleServiceImpl.java
@@ -1,7 +1,5 @@
 package com.spring.temp.domain.service.impl;
 
-import com.spring.temp.domain.dto.ExampleDto;
-import com.spring.temp.domain.mapper.ExampleMapper;
 import com.spring.temp.domain.model.UserActions;
 import com.spring.temp.domain.service.ExampleService;
 import lombok.RequiredArgsConstructor;
@@ -14,12 +12,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class ExampleServiceImpl implements ExampleService {
 
-    private final ExampleMapper exampleMapper;
     @Override
     public void getAll() {
         UserActions userActions = new UserActions();
-        // Mapper usage
-        ExampleDto exampleDto = exampleMapper.userActionToExampleDto(userActions);
         // Repository actions etc.
     }
 }
